35d96f8013dfe3c9a9a9d1d4a738a991fd708dcc
Doug SImpson <doug.simpson@adasphere.com>
2019-05-10 15:27:57 -0400

    Clean up Extensions.cs AB#14168

--- Ada.Xamarin.FileStorage.Abstractions/Extensions.cs
+++ Ada.Xamarin.FileStorage.Abstractions/Extensions.cs
@@ -1,22 +1,20 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
 
 namespace Ada.Xamarin.FileStorage.Abstractions
 {
     public static class Extensions
     {
-        public static byte[] CleanByteOrderMark(this byte[] i_Bytes)
+        public static byte[] CleanByteOrderMark(this byte[] bytes)
         {
             byte[] bom = new byte[] { 0xEF, 0xBB, 0xBF };
 
-            if (i_Bytes.Take(3).SequenceEqual(bom))
+            if (bytes.Take(3).SequenceEqual(bom))
             {
-                return i_Bytes.Skip(3).ToArray();
+                return bytes.Skip(3).ToArray();
             }
 
-            return i_Bytes;
+            return bytes;
         }
     }
 }
