4d4f934d1be39044624e4f50fb9e1459076b39d5
Doug SImpson <doug.simpson@adasphere.com>
2019-05-10 15:35:58 -0400

    AB#14168 Adds comments to extensions

--- Ada.Xamarin.FileStorage.Abstractions/Extensions.cs
+++ Ada.Xamarin.FileStorage.Abstractions/Extensions.cs
@@ -9,6 +9,8 @@ namespace Ada.Xamarin.FileStorage.Abstractions
         {
             byte[] bom = new byte[] { 0xEF, 0xBB, 0xBF };
 
+            // If the first 3 bytes are byte order marker, 
+            // then this pulls them out of the array
             if (bytes.Take(3).SequenceEqual(bom))
             {
                 return bytes.Skip(3).ToArray();
