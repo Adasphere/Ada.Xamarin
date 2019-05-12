1fec0fbe1b168188e754b0d703f129384bbf56bc
Doug SImpson <doug.simpson@adasphere.com>
2019-05-12 14:21:08 -0400

    Excluded ExtensionsTests from code coverage analysis

--- Ada.Xamarin.FileStorage.Abstractions.Tests/ExtensionsTests.cs
+++ Ada.Xamarin.FileStorage.Abstractions.Tests/ExtensionsTests.cs
@@ -1,8 +1,10 @@
 using System;
+using System.Diagnostics.CodeAnalysis;
 using Xunit;
 
 namespace Ada.Xamarin.FileStorage.Abstractions.Tests
 {
+    [ExcludeFromCodeCoverage]
     public class ExtensionsTests
     {
         [Fact]
